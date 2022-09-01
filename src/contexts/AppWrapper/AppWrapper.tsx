d9bbf23eb59aae5d9fb29fa98d9f5302d2be2abd
Allan Oliveira <nallamo2016@gmail.com>
2022-09-01 18:04:23 -0300

    feat: connect locale provider with main app provider wrapper

--- src/contexts/AppWrapper/AppWrapper.tsx
+++ src/contexts/AppWrapper/AppWrapper.tsx
@@ -2,12 +2,16 @@ import { ReactNode } from 'react'
 
 import { ThemeProvider } from '@granosafe/design-system'
 
+import { LocaleProvider } from 'contexts/LocaleContext'
+
 type AppWrapperProps = {
   children: ReactNode
 }
 
 const AppWrapper = ({ children }: AppWrapperProps) => (
-  <ThemeProvider>{children}</ThemeProvider>
+  <ThemeProvider>
+    <LocaleProvider>{children}</LocaleProvider>
+  </ThemeProvider>
 )
 
 export default AppWrapper
